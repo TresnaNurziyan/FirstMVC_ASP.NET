0456588c7fb8e7a300492aa51fa0c4f5b465d27e
Tresna Nurziyan <tresna.nurziyan@gmail.com>
2017-07-26 12:01:36 +0700

    First MVC ASP.NET

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /HelloWorld/Welcome
+        // HttpServerUtility.HtmlEncode to protect from malicious input (Javascript)
+        public ActionResult Welcome(string name = "Tresna", int NumTimes = 4)
+        {
+            ViewBag.Message = "Hello" + name;
+            ViewBag.NumTimes = NumTimes;
+
+            return View();
+        }
+    }
+}
